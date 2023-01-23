44f349a3d862adbef275fe869c8c1aa7025a7e38
Nikita Shadrin <shadrinnikita@gmail.com>
2023-01-23 04:22:05 +0200

    new file:   .eslintrc.js 	new file:   .eslintrc.json 	new file:   .gitignore 	new file:   README.md 	new file:   app/head.js 	new file:   app/layout.js 	new file:   app/page.js 	new file:   components/CustomTexts.jsx 	new file:   components/ExploreCard.jsx 	new file:   components/Footer.jsx 	new file:   components/InsightCard.jsx 	new file:   components/Navbar.jsx 	new file:   components/NewFeatures.jsx 	new file:   components/StartSteps.jsx 	new file:   components/index.js 	new file:   constants/index.js 	new file:   next.config.js 	new file:   package-lock.json 	new file:   package.json 	new file:   pages/api/hello.js 	new file:   postcss.config.js 	new file:   public/arrow-down.svg 	new file:   public/arrow.svg 	new file:   public/cover.png 	new file:   public/facebook.svg 	new file:   public/favicon.ico 	new file:   public/get-started.png 	new file:   public/headset.svg 	new file:   public/instagram.svg 	new file:   public/linkedin.svg 	new file:   public/map.png 	new file:   public/menu.svg 	new file:   public/people-01.png 	new file:   public/people-02.png 	new file:   public/people-03.png 	new file:   public/planet-01.png 	new file:   public/planet-02.png 	new file:   public/planet-03.png 	new file:   public/planet-04.png 	new file:   public/planet-05.png 	new file:   public/planet-06.png 	new file:   public/planet-07.png 	new file:   public/planet-08.png 	new file:   public/planet-09.png 	new file:   public/search.svg 	new file:   public/stamp.png 	new file:   public/twitter.svg 	new file:   public/vercel.svg 	new file:   public/vrpano.svg 	new file:   public/whats-new.png 	new file:   sections/About.jsx 	new file:   sections/Explore.jsx 	new file:   sections/Feedback.jsx 	new file:   sections/GetStarted.jsx 	new file:   sections/Hero.jsx 	new file:   sections/Insights.jsx 	new file:   sections/WhatsNew.jsx 	new file:   sections/World.jsx 	new file:   sections/index.js 	new file:   styles/globals.css 	new file:   styles/index.js 	new file:   tailwind.config.js 	new file:   utils/motion.js

---
+++ constants/index.js
@@ -0,0 +1,88 @@
+export const exploreWorlds = [
+  {
+    id: 'world-1',
+    imgUrl: '/planet-01.png',
+    title: 'Hogwarts',
+  },
+  {
+    id: 'world-2',
+    imgUrl: '/planet-02.png',
+    title: 'The Upside Down',
+  },
+  {
+    id: 'world-3',
+    imgUrl: '/planet-03.png',
+    title: 'Kadirojo Permai',
+  },
+  {
+    id: 'world-4',
+    imgUrl: '/planet-04.png',
+    title: 'Paradise Island',
+  },
+  {
+    id: 'world-5',
+    imgUrl: '/planet-05.png',
+    title: 'Hawkins Labs',
+  },
+];
+
+export const startingFeatures = [
+  'Download and install Metaversus',
+  'Find a world that appeals to you',
+  'Enter it and enjoy the madness!',
+];
+
+export const newFeatures = [
+  {
+    imgUrl: '/vrpano.svg',
+    title: 'New Worlds',
+    subtitle:
+        'The latest update offers many more worlds to explore than ever before!',
+  },
+  {
+    imgUrl: '/headset.svg',
+    title: 'Improved graphics',
+    subtitle:
+        'The visual update significanly increased graphical fidelity. The metaverse is more realistic than ever before!',
+  },
+];
+
+export const insights = [
+  {
+    imgUrl: '/planet-06.png',
+    title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit.',
+    subtitle:
+        'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean eget velit id nibh tempor accumsan',
+  },
+  {
+    imgUrl: '/planet-07.png',
+    title: 'Lorem ipsum 2: Electric Boogaloo',
+    subtitle:
+        'Donec a est a ligula volutpat semper. Vivamus ultrices eros odio, id euismod mauris commodo et. Integer placerat, augue non commodo lacinia',
+  },
+  {
+    imgUrl: '/planet-08.png',
+    title: 'More lorem ipsum dolor sit amet',
+    subtitle:
+        'You get it, this is just filler text, I could fill it with lyrics to Chop Suey by System of a Down if I wanted to and it would not matter at all',
+  },
+];
+
+export const socials = [
+  {
+    name: 'twitter',
+    url: '/twitter.svg',
+  },
+  {
+    name: 'linkedin',
+    url: '/linkedin.svg',
+  },
+  {
+    name: 'instagram',
+    url: '/instagram.svg',
+  },
+  {
+    name: 'facebook',
+    url: '/facebook.svg',
+  },
+];
---
+++ sections/About.jsx
@@ -0,0 +1,49 @@
+'use client';
+
+import { motion } from 'framer-motion';
+import { TypingText } from '../components';
+
+import styles from '../styles';
+import { fadeIn, staggerContainer } from '../utils/motion';
+
+const About = () => (
+  <section className={`${styles.paddings} relative z-10`}>
+    <div className="gradient-02 z-0" />
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: false, amount: 0.25 }}
+      className={`${styles.innerWidth} mx-auto ${styles.flexCenter} flex-col`}
+    >
+      <TypingText title="| About Metaversus" textStyles="text-center" />
+
+      <motion.p
+        variants={fadeIn('up', 'tween', 0.2, 1)}
+        className="mt-[8px] font-normal sm:text-[32px] text-[20px] text-center text-secondary-white"
+      >
+        <span className="font-extrabold text-white">Metaverse</span> is the next step in the internet's evolution,
+        a digital space where businesses can interact with customers in new ways,
+        creating new revenue streams. It is a game-changer that can{' '}
+        <span className="font-extrabold text-white">
+          revolutionize
+        </span>{' '}
+        the way we live, work, and play. Using{' '}
+        <span className="font-extrabold text-white">VR</span> technology you can
+        easily explore the metaverse and turn your dreams into
+        reality.{' '}
+        <span className="font-extrabold text-white">Explore</span> the madness
+        of the metaverse by scrolling down
+      </motion.p>
+
+      <motion.img
+        variants={fadeIn('up', 'tween', 0.3, 1)}
+        src="/arrow-down.svg"
+        alt="arrow down"
+        className="w-[18px] h-[28px] object-contain mt-[28px]"
+      />
+    </motion.div>
+  </section>
+);
+
+export default About;
